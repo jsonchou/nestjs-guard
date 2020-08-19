be46f3930b1cc6dd0bcb1af72efd1b4f64d1c92b
json chou <jsonchou329@gmail.com>
2020-08-19 17:11:36 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Fastify sample
+### sample
 
 Note that if you are running the Nest app on a remote machine, you may need to change the listen address, as per [these instructions](https://docs.nestjs.com/techniques/performance#adapter):
 
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -5,7 +5,7 @@ import { USER_TOKEN_NAME } from '../constants/index'
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    console.log(`Cache request...`);
+    console.log(`Catch request...`);
     next();
   }
 }
